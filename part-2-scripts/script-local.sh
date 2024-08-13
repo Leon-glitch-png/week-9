9b6715561154c44fd044de289d2b6b3e6bff456b
Leon-glitch-png <rose394857@gmail.com>
2024-08-13 16:25:24 +0530

    added week9

--- part-2-scripts/script-local.sh
+++ part-2-scripts/script-local.sh
@@ -1,1 +1,1 @@
-ssh -t -i "week_9.pem" ubuntu@http://ec2-13-127-175-187.ap-south-1.compute.amazonaws.com/  "sudo bash ~/deploy.sh"
+ssh -t -i "week9.pem" ubuntu@ec2-65-0-199-72.ap-south-1.compute.amazonaws.com "sudo bash ~/deploy.sh"
